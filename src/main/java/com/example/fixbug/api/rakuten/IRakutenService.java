544b17bfef28c8887210261f46add8bfb9baccb7
Đinh Văn Dũng <dungdv@watermelon.vn>
2022-03-07 15:26:01 +0900

    test

--- src/main/java/com/example/fixbug/api/rakuten/IRakutenService.java
+++ src/main/java/com/example/fixbug/api/rakuten/IRakutenService.java
@@ -4,15 +4,11 @@ import com.example.fixbug.api.requesthelper.RetrofitClient;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.*;
 
 import java.util.Map;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.QueryMap;
-
-import java.util.Map;
 
 public interface IRakutenService {
     IRakutenService SERVICE = RetrofitClient.getInstance().getService("https://api.rms.rakuten.co.jp/", IRakutenService.class);
@@ -20,5 +16,8 @@ public interface IRakutenService {
     IRakutenService SERVICEDEV = RetrofitClient.getInstance().getService("https://app.rakuten.co.jp/services/", IRakutenService.class);
 
     @GET("/es/1.0/item/search")
-    Call<ResponseBody> search(@Header("Authorization") String authorization, @Body RequestBody request);
+    Call<ResponseBody> search(@Header("Authorization") String authorization, @QueryMap Map<String,String> request);
+
+    @GET("api/IchibaItem/Search/20170706")
+    Call<IchibaResponse> searchIchiba(@QueryMap Map<String, String> query);
 }
